ba6448226b8456de50a3120166613971653ee3a8
Tomer Klein <tomer.klein@gmail.com>
2023-01-14 01:38:15 +0200

    Update setup.py and added project urls and readme content

--- setup.py
+++ setup.py
@@ -1,31 +1,44 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+with open("README.md", "r", encoding="UTF-8") as f:
+     readme = f.read()
 
 
 setup_args = dict(
     name='ma-nish',
-    version='1.0.0',
+    version='0.3.0',
     description='ma-nish is an unofficial python wrapper for Whatsapp cloud api',
     long_description_content_type="text/markdown",
+    long_description=readme,
     license='MIT',
     packages=find_packages(),
     author='Tomer Klein',
     author_email='tomer.klein@gmail.com',
     keywords=['meta', 'ma-nish', 'facebook','whatsapp'],
     url='https://github.com/t0mer/ma-nish',
-    download_url='https://pypi.org/project/ma-nish/'
+    download_url='https://pypi.org/project/ma-nish/',
+    project_urls={
+        "Documentation": "https://github.com/t0mer/ma-nish",
+        "Source": "https://github.com/t0mer/ma-nish",
+    },
 )
 
+
+
 install_requires = [
-    'python-dotenv>=0.21.0',
-    'requests>=2.28.1',
-    'requests-toolbelt>=0.10.1',
-    'validators>=0.20.0',
-    'geopy>=2.3.0',
-    'Pillow>=9.3.0',
-    'loguru==0.6.0',
-    'uvicorn==0.20.0',
-    'fastapi==0.88.0',
-    'jinja2==1.2 ',
+    'python-dotenv',
+    'requests',
+    'requests-toolbelt',
+    'validators',
+    'geopy',
+    'Pillow',
+    'loguru',
+    'uvicorn',
+    'fastapi',
+    'jinja2',
+    'dataclasses-json',
     ]
 
 if __name__ == '__main__':
